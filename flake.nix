4ec42dc3d01d83a749177396a5b85e171b8353e6
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-01-31 09:28:26 -0300

    Criação de flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "SoloRPG flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            dotnet-sdk_9
+            dotnet-runtime_9
+            dotnet-aspnetcore_9
+          ];
+        };
+      }
+    );
+}
