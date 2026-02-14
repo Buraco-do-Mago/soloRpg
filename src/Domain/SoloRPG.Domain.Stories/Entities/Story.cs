755db25badbe75325eb3deec5f3a4d71d9e2c17e
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-14 09:35:51 -0300

    Adding a few more fields to the Story entity

--- src/Domain/SoloRPG.Domain.Stories/Entities/Story.cs
+++ src/Domain/SoloRPG.Domain.Stories/Entities/Story.cs
@@ -5,6 +5,8 @@ namespace SoloRPG.Domain.Stories.Entities;
 public class Story : IEntity
 {
     public Guid? Id { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public DateTime? LastUpdatedAt { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required string Prompt { get; set; }
