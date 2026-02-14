9056cf75d1a6203bf45845273eb5e85526b99267
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-14 09:36:02 -0300

    Adding the new entities to the database

--- src/Application/SoloRPG.Application/Data/Database.cs
+++ src/Application/SoloRPG.Application/Data/Database.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using SoloRPG.Domain.Sheets.Entities;
+using SoloRPG.Domain.Stories.Entities;
 
 namespace SoloRPG.Application.Data;
 
 public class Database(DbContextOptions<Database> options) : DbContext(options)
 {
     public DbSet<Sheet> Sheets { get; set; }
+    public DbSet<Story> Stories { get; set; }
+    public DbSet<Chapter> Chapters { get; set; }
+    public DbSet<Scene> Scenes { get; set; }
 }
