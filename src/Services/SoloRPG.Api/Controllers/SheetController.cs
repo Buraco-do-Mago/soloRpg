799f9834b9b30962125e3d9e48fdec4cd2b5f0f9
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-14 09:17:29 -0300

    Adding a get controller to get sheets

--- src/Services/SoloRPG.Api/Controllers/SheetController.cs
+++ src/Services/SoloRPG.Api/Controllers/SheetController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SoloRPG.Domain.Sheets.Commands;
+using SoloRPG.Domain.Sheets.Entities;
 using SoloRPG.Domain.Sheets.Services;
 
 namespace SoloRPG.Api.Controllers;
 
 [ApiController]
 [Route("api/sheets")]
-public class SheetController(SheetCreationService sheetCreationService) : ControllerBase
+public class SheetController(SheetCreationService sheetCreationService, DbContext dbContext) : ControllerBase
 {
     [HttpPost]
     public IActionResult Create([FromBody] CreateSheetCommand command)
@@ -14,5 +16,18 @@ public class SheetController(SheetCreationService sheetCreationService) : Contro
         var playerId = Guid.NewGuid();
         var character = sheetCreationService.CreateSheet(playerId, command);
         return Ok(character);
+    }
+
+    [HttpGet("/api/sheets/{id}")]
+    public async Task<IActionResult> GetAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var sheet = await dbContext
+            .Set<Sheet>()
+            .Include(sheet => sheet.Attributes)
+            .Include(sheet => sheet.Skills)
+            .FirstOrDefaultAsync(sheet => sheet.Id == id, cancellationToken);
+        if (sheet is null)
+            return NotFound();
+        return Ok(sheet);
     }
 }
