7a5b814dc04d1bd8e6db1cac751df10054c6e413
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-15 10:56:08 -0300

    Renaming property to make it more clear

--- src/Domain/SoloRPG.Domain.Chats/Entities/Entry.cs
+++ src/Domain/SoloRPG.Domain.Chats/Entities/Entry.cs
@@ -7,7 +7,7 @@ namespace SoloRPG.Domain.Chats.Entities;
 public class Entry : IEntity
 {
     public Guid? Id { get; set; }
-    public SourceType Source { get; set; }
+    public SourceType SourceType { get; set; }
     public Character? Character { get; set; }
     public required DateTime CreatedAt { get; set; }
     public required string Content { get; set; }
