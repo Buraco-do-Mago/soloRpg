68c0abbeb55f7a675c97f5b4b7890e77e8d20cee
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-15 10:53:55 -0300

    Adding a story reference to each campaign

--- src/Domain/SoloRPG.Domain.Campaigns/Entities/Campaign.cs
+++ src/Domain/SoloRPG.Domain.Campaigns/Entities/Campaign.cs
@@ -1,3 +1,4 @@
+using SoloRPG.Domain.Stories.Entities;
 using SoloRPG.Infra.Interfaces;
 
 namespace SoloRPG.Domain.Campaigns.Entities;
@@ -5,6 +6,7 @@ namespace SoloRPG.Domain.Campaigns.Entities;
 public class Campaign : IEntity
 {
     public Guid? Id { get; set; }
+    public required Story Story { get; set; }
     public required DateTime CreatedAt { get; set; }
     public List<Session>? Sessions { get; set; }
 }
