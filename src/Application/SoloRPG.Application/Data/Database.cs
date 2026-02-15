ada8204d81aabcdfc4c178119599c3cabe43da2d
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-02-15 10:37:08 -0300

    Adding missing entities to the DbContext

--- src/Application/SoloRPG.Application/Data/Database.cs
+++ src/Application/SoloRPG.Application/Data/Database.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using SoloRPG.Domain.Campaigns.Entities;
+using SoloRPG.Domain.Characters.Entities;
+using SoloRPG.Domain.Chats.Entities;
 using SoloRPG.Domain.Sheets.Entities;
 using SoloRPG.Domain.Stories.Entities;
 
@@ -10,4 +13,9 @@ public class Database(DbContextOptions<Database> options) : DbContext(options)
     public DbSet<Story> Stories { get; set; }
     public DbSet<Chapter> Chapters { get; set; }
     public DbSet<Scene> Scenes { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+    public DbSet<Entry> ChatEntries { get; set; }
+    public DbSet<Campaign> Campaigns { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<Character> Characters { get; set; }
 }
